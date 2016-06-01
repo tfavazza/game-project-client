2dbc9e09d5b022232f0d99c49bf7ec065852e03e
tfavazza <tara.favazza@gmail.com>
2016-06-01 16:51:02 -0400

    api is fixed for sign up!
    because /s are important!

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -4,7 +4,7 @@ const app = require('../app.js');
 const signUp = function (data) {
   return $.ajax(
      {
-      url: app.host + '/sign-up',
+      url: app.host + '/sign-up/',
       method: 'POST',
       data: data,
     });
