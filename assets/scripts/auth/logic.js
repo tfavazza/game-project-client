17ab129a0856b23ad4787b6d3b53317a2dfdf890
tfavazza <tara.favazza@gmail.com>
2016-06-02 10:04:10 -0400

    wins are tracked now!
    this is on the wrong branch though.

--- assets/scripts/auth/logic.js
+++ assets/scripts/auth/logic.js
@@ -8,30 +8,38 @@ const addTheButtonBack = function() {
   document.getElementById('hide-modal-button').innerHTML = '<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Game Over!</button>';
 };
 
-const checkForWins = function(arr) { //FIXME only one win state right now
-  console.log(arr);
-  if(arr[0] === arr[1] && arr[1] === arr[2]) {
-    if(arr[0] === "X") {
-      console.log("player 1 wins!"); //TODO remove console.logs
-      addTheButtonBack();
-    } else {
-      console.log("player 2 wins!"); //TODO remove console.logs
-      addTheButtonBack();
+const aWinHappened = function(piece1, piece2, piece3) {
+    console.log("were checking for wins!");
+    let arr = winArray;
+    let spot1 = arr[piece1];
+    if (spot1 === null) {
+      return false;
     }
-  }
-}; /*
-win conditions
-all the same:
-[0,1,2] n, n+1, n+2
-[0,4,6] n, n+4, n+6
-[0,4,8] n, n+4, n+8
-[1,4,7] n, n+3, n+6
-[2,4,6] n, n+2, n+4
-[2,5,8] n, n+3, n+6
-[3,4,5] n, n+1, n+2
-[6,7,8] n, n+1, n+2
-*/
-//final test to end the game
+    var spot2 = arr[piece2];
+    if (spot1 !== spot2) {
+      return false;
+    }
+    var spot3 = arr[piece3];
+    if (spot1 !== spot3) {
+      return false;
+    }
+      console.log("Player " + spot1 + " wins!");
+      addTheButtonBack();
+    return true;
+  };
+  const checkForWins = function(arr) {
+    console.log(arr);
+    aWinHappened(0, 1, 2);
+    aWinHappened(0, 3, 6);
+    aWinHappened(0, 4, 8);
+    aWinHappened(1, 4, 7);
+    aWinHappened(2, 4, 6);
+    aWinHappened(2, 5, 8);
+    aWinHappened(3, 4, 5);
+    aWinHappened(6, 7, 8);
+  };
+
+  //final test to end the game
 
 
 const togglePlayer = function() {
@@ -49,28 +57,28 @@ const getButtonId = function(){
       buttonNumber = "0";
       break;
     case '#one':
-      buttonNumber ="1";
+      buttonNumber = "1";
       break;
     case '#two':
-      buttonNumber ="2";
+      buttonNumber = "2";
       break;
     case '#three':
-      buttonNumber ="3";
+      buttonNumber = "3";
       break;
     case '#four':
-      buttonNumber ="4";
+      buttonNumber = "4";
       break;
     case '#five':
-      buttonNumber ="5";
+      buttonNumber = "5";
       break;
     case '#six':
-      buttonNumber ="6";
+      buttonNumber = "6";
       break;
     case '#seven':
-      buttonNumber ="7";
+      buttonNumber = "7";
       break;
     case '#eight':
-      buttonNumber ="8";
+      buttonNumber = "8";
       break;
   }
   return buttonNumber;
